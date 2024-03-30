2dbe6e59aa0505ce84442cd432a66ff5e766f485
Ashok Bera <ashokkumarbera1999@gmail.com>
2024-03-31 00:13:53 +0530

    Update EventMessageDTO.java

--- worker/src/main/java/com/swiftbank/worker/dtos/EventMessageDTO.java
+++ worker/src/main/java/com/swiftbank/worker/dtos/EventMessageDTO.java
@@ -3,22 +3,23 @@ package com.swiftbank.worker.dtos;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.swiftbank.worker.enums.NotificationType;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
-import org.springframework.core.io.FileSystemResource;
 
 import java.io.File;
 
 @Data
 @ToString
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EventMessageDTO {
-    private final NotificationType type;
-    private final String mobile;
-    private final String email;
-    private final String subject;
-    private final String body;
-    private final String fileName;
-    private final File file;
+    private NotificationType type;
+    private String mobile;
+    private String email;
+    private String subject;
+    private String body;
+    private String fileName;
+    private File file;
 }
